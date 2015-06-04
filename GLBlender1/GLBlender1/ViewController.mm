37171edd281d92007789943431dc736770bc0065
Son Nguyen Kim <nguyenkims@users.noreply.github.com>
2015-06-04 16:13:15 +0200

    par texels and normals

--- GLBlender1/GLBlender1/ViewController.mm
+++ GLBlender1/GLBlender1/ViewController.mm
@@ -14,6 +14,9 @@
 {
     int nbVertices;
     float verticePositions[108];
+    float verticesTexels[72];
+    float verticesNormals[108];
+    float   _rotate;
 }
 
 @property (strong, nonatomic) GLKBaseEffect* effect;
@@ -39,16 +42,11 @@
     
     // OpenGL ES Settings
     glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-    
     glEnable(GL_CULL_FACE);
 }
 
 - (void) parseObj
 {
-    char x[] = "abcd";
-    char* a;
-    a = x;
-    
     NSString* path = [[NSBundle mainBundle] pathForResource:@"cube.obj" ofType:nil];
     
     Model model = getOBJinfo((char*)[path UTF8String]);
@@ -84,6 +82,42 @@
         verticePositions[9*i+7] = positions[vC][1] ;
         verticePositions[9*i+8] = positions[vC][2] ;
     }
+    
+    for(int i=0; i<model.faces; i++)
+    {
+        int vtA = faces[i][1] - 1;
+        int vtB = faces[i][4] - 1;
+        int vtC = faces[i][7] - 1;
+        
+        verticesTexels[6*i]= texels[vtA][0];
+        verticesTexels[6*i+1]= texels[vtA][1];
+        
+        verticesTexels[6*i+2]= texels[vtB][0];
+        verticesTexels[6*i+3]= texels[vtB][1];
+
+        verticesTexels[6*i+4]= texels[vtC][0];
+        verticesTexels[6*i+5]= texels[vtC][1];
+    }
+    
+    for(int i=0; i<model.faces; i++)
+    {
+        int vnA = faces[i][2] - 1;
+        int vnB = faces[i][5] - 1;
+        int vnC = faces[i][8] - 1;
+        
+        verticesNormals[9*i] = normals[vnA][0];
+        verticesNormals[9*i+1] = normals[vnA][1];
+        verticesNormals[9*i+2] = normals[vnA][2];
+        
+        verticesNormals[9*i+3] = normals[vnB][0];
+        verticesNormals[9*i+4] = normals[vnB][1];
+        verticesNormals[9*i+5] = normals[vnB][2];
+        
+        verticesNormals[9*i+6] = normals[vnC][0];
+        verticesNormals[9*i+7] = normals[vnC][1];
+        verticesNormals[9*i+8] = normals[vnC][2];
+    }
+
 
 
     
@@ -94,6 +128,24 @@
 {
     // Initialize
     self.effect = [[GLKBaseEffect alloc] init];
+    
+    // Texture
+    NSDictionary* options = @{ GLKTextureLoaderOriginBottomLeft: @YES };
+    NSError* error;
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"cube.png" ofType:nil];
+    GLKTextureInfo* texture = [GLKTextureLoader textureWithContentsOfFile:path options:options error:&error];
+    
+    if(texture == nil)
+        NSLog(@"Error loading file: %@", [error localizedDescription]);
+    
+    self.effect.texture2d0.name = texture.name;
+    self.effect.texture2d0.enabled = true;
+    
+    // Light
+    self.effect.light0.enabled = GL_TRUE;
+    self.effect.light0.position = GLKVector4Make(1.0f, 1.0f, 1.0f, 1.0f);
+    self.effect.lightingType = GLKLightingTypePerPixel;
+
 }
 
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
@@ -110,6 +162,14 @@
     // Positions
     glEnableVertexAttribArray(GLKVertexAttribPosition);
     glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, 0, verticePositions);
+    
+    // Texels
+    glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
+    glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, 0, verticesTexels);
+    
+    // Normals
+    glEnableVertexAttribArray(GLKVertexAttribNormal);
+    glVertexAttribPointer(GLKVertexAttribNormal, 3, GL_FLOAT, GL_FALSE, 0, verticesNormals);
     
     // 2
     // Draw Model
@@ -128,8 +188,16 @@
     GLKMatrix4 modelViewMatrix = GLKMatrix4Identity;
     modelViewMatrix = GLKMatrix4Translate(modelViewMatrix, 0.0f, 0.0f, -5.0f);
     modelViewMatrix = GLKMatrix4RotateX(modelViewMatrix, GLKMathDegreesToRadians(45.0f));
+    modelViewMatrix = GLKMatrix4RotateY(modelViewMatrix, GLKMathDegreesToRadians(_rotate));
+    modelViewMatrix = GLKMatrix4RotateZ(modelViewMatrix, GLKMathDegreesToRadians(_rotate));
     self.effect.transform.modelviewMatrix = modelViewMatrix;
 }
+
+- (void)update
+{
+    _rotate += 1.0f;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
